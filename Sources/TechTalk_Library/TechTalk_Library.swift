0d1ac8266d90a473634df946bad9259ca8ced3cd
Yen Pheayuth <yenpheayuth2222@gmail.com>
2023-07-07 22:40:48 +0700

    Update function

--- Sources/TechTalk_Library/TechTalk_Library.swift
+++ Sources/TechTalk_Library/TechTalk_Library.swift
@@ -1,6 +1,12 @@
+import UIKit
 public struct TechTalk_Library {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+    
+    public func customAlertPop(titleValue: String, mesString: String, viewController: UIViewController) {
+        viewController.customOneAlert(image: "", title: titleValue, message: mesString, yesTitle: "OK")
+    }
+    
 }
