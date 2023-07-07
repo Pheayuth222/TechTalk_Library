a7290c64fe61481bc50673c13a39f9c6f6494127
yenpheayuth2222 <yenpheayuth2222@gmail.com>
2023-07-07 10:21:51 +0700

    Add New Update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TechTalk_Library",
-    platforms: [
+    platforms: [ // 1. Add platforms
         .iOS(.v11)
     ],
     products: [
